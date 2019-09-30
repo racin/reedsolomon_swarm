87254778ede53a44c0df357fcafb9e5e799e06e8
Racin Nygaard <racin.nygaard@gmail.com>
2019-09-30 10:38:48 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/racin/reedsolomon_swarm
+
+go 1.13
+
+require github.com/klauspost/reedsolomon v1.9.3
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/klauspost/reedsolomon"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+}
